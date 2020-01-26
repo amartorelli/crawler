defe61d9b9ad0dd4261bb384f34e4c489f664d0c
Alessio Martorelli <alessio.martorelli@gmail.com>
2020-01-26 21:24:40 +0000

    added conditional fetch (any domain - domain only)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate lazy_static;
 extern crate clap;
 extern crate queues;
 extern crate regex;
@@ -19,10 +21,12 @@ struct Crawler {
     visited: HashSet<String>,
     queue: Queue<String>,
     base_url: Url,
+    fetch_any_domain: bool,
+    domain_regex: Regex,
 }
 
 impl Crawler {
-    fn new(target: &String) -> Crawler {
+    fn new(target: &String, any_domain: bool) -> Crawler {
         let client = reqwest::Client::new();
         let visited: HashSet<String> = HashSet::new();
         let queue: Queue<String> = queue![];
@@ -33,39 +37,54 @@ impl Crawler {
             Err(e) => panic!("{}", e),
         }
 
+        let mut rule = String::from("^");
+        rule.push_str(burl.as_str());
+        rule.push_str(".*");
         Crawler {
             client: client,
             target: target.to_string(),
             visited: visited,
             queue: queue,
             base_url: burl,
+            fetch_any_domain: any_domain,
+            domain_regex: Regex::new(rule.as_str()).unwrap(),
         }
     }
 
     #[tokio::main]
-    async fn fetch(&self) -> Result<String, reqwest::Error> {
-        let body = self
-            .client
-            .get(self.target.as_str())
-            .send()
-            .await?
-            .text()
-            .await?;
+    async fn fetch(&self, target: &str) -> Result<String, reqwest::Error> {
+        let body = self.client.get(target).send().await?.text().await?;
         Ok(body)
+    }
+
+    fn should_fetch(&self, link: &str) -> bool {
+        if self.fetch_any_domain || self.domain_regex.is_match(link) {
+            println!("Comparing {}/{:#?} should fetch", link, self.domain_regex);
+            return true;
+        }
+        println!(
+            "Comparing {}/{:#?} should not fetch",
+            link, self.domain_regex
+        );
+        println!("Domain {} should not fetch", link);
+        false
     }
 
     fn run(&mut self) {
         match self
             .queue
-            .add(self.convert_link_to_abs(self.base_url.as_str()))
+            .add(self.convert_link_to_abs(self.target.as_str()))
         {
             Err(e) => println!("{}", e),
             _ => (),
         }
         while self.queue.size() > 0 {
-            match self.fetch() {
-                Ok(content) => match self.get_links(content) {
-                    Ok(()) => println!("added links: {:#?}", self.queue),
+            match self.queue.remove() {
+                Ok(link) => match self.fetch(link.as_str()) {
+                    Ok(content) => match self.get_links(content) {
+                        Ok(()) => println!("added links: {:#?}", self.queue),
+                        Err(e) => println!("{}", e),
+                    },
                     Err(e) => println!("{}", e),
                 },
                 Err(e) => println!("{}", e),
@@ -74,8 +93,10 @@ impl Crawler {
     }
 
     fn convert_link_to_abs(&self, link: &str) -> String {
-        let re = Regex::new(r"^http://.*").unwrap();
-        if !re.is_match(link) {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"^http://.*").unwrap();
+        }
+        if !RE.is_match(link) {
             match self.base_url.join(link) {
                 Ok(full_url) => return full_url.into_string(),
                 Err(e) => {
@@ -96,11 +117,13 @@ impl Crawler {
                 let link = &String::from(l);
                 let full_link = self.convert_link_to_abs(link);
                 if !self.visited.contains(&full_link) {
-                    match self.queue.add(full_link.to_string()) {
-                        Err(e) => println!("{}", e),
-                        _ => (),
+                    if self.should_fetch(&full_link) {
+                        match self.queue.add(full_link.to_string()) {
+                            Err(e) => println!("{}", e),
+                            _ => (),
+                        }
+                        self.visited.insert(full_link.to_string());
                     }
-                    self.visited.insert(full_link.to_string());
                 }
             });
         Ok(())
@@ -117,11 +140,17 @@ fn main() {
                 .required(true)
                 .short("t")
                 .takes_value(true),
-        );
+        )
+        .arg(Arg::with_name("any").help("fetch any domain").short("a"));
     let matches = opts.get_matches();
 
+    let mut any_domain = false;
+    if let Some(_a) = matches.value_of("any") {
+        any_domain = true;
+    }
+
     if let Some(t) = matches.value_of("target") {
-        let mut crawler: Crawler = Crawler::new(&t.to_string());
+        let mut crawler: Crawler = Crawler::new(&t.to_string(), any_domain);
         crawler.run();
     } else {
         println!("unable to parse target");
